777fe82fd2be8bb602374a72df2d1e06e5bc9299
Jawad Charles <jawadcharls@gmail.com>
2025-10-16 22:14:33 +0300

    Git boost

--- resources/views/cameralog.blade.php
+++ resources/views/cameralog.blade.php
@@ -228,7 +228,7 @@
                     modalMessage.innerHTML = `❌ Could not mark attended`;
                 }
             } catch (err) {
-                modalMessage.textContent = `❌ Error verifying`;
+                modalMessage.textContent = `❌ Error verifying guest`;
                 console.error(err);
             }
         });
